c4634c6aab87908cdf01c98271e976ecbf6d400d
suzanneazzam <suzanne.azzam@gmail.com>
2022-09-23 17:12:35 -0700

    completed assignment

---
+++ destructuring.js
@@ -0,0 +1,74 @@
+// What does the following code return/print?
+let facts = { numPlanets: 8, yearNeptuneDiscovered: 1846 };
+let { numPlanets, yearNeptuneDiscovered } = facts;
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
+
+// What does the following code return/print?
+let planetFacts = {
+  numPlanets: 8,
+  yearNeptuneDiscovered: 1846,
+  yearMarsDiscovered: 1659,
+};
+let { numPlanets, ...discoveryYears } = planetFacts;
+
+console.log(discoveryYears);
+// {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+// What does the following code return/print?
+function getUserData({ firstName, favoriteColor = "green" }) {
+  return `Your name is ${firstName} and you like ${favoriteColor}`;
+}
+getUserData({ firstName: "Alejandro", favoriteColor: "purple" });
+// Your name is Alejandro and you like purple
+getUserData({ firstName: "Melissa" });
+// Your name is Melissa and you like green
+getUserData({});
+// Your name is undefined and you like green
+
+// What does the following code return/print?
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+console.log(first); // Maya
+console.log(second); // Marisa
+console.log(third); // Chi
+
+// What does the following code return/print?
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+  "Raindrops on roses",
+  "whiskers on kittens",
+  "Bright copper kettles",
+  "warm woolen mittens",
+  "Brown paper packages tied up with strings",
+];
+console.log(raindrops); // "Raindrops on roses"
+console.log(whiskers); // "whiskers on kittens"
+console.log(aFewOfMyFavoriteThings); // ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+// What does the following code return/print?
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]];
+console.log(numbers); // [10, 30, 20]
+
+// ES2015 Object Destructuring
+const obj = {
+  numbers: {
+    a: 1,
+    b: 2,
+  },
+};
+const {
+  numbers: { a, b },
+} = obj;
+
+// the solution says: const {a,b} = obj.numbers, would my answer work too?
+
+// ES2015 One-Line Array Swap with Destructuring
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+// raceResults()
+const raceResults = ([first, second, third, ...rest]) => ({
+  first,
+  second,
+  third,
+  rest,
+});
